2a037f0a9b7a65613cd9e8a1338f616a6080967a
wagner <wagner.picchioni@gmail.com>
2020-08-10 15:47:18 -0300

    first commit

---
+++ public class Veiculo {.java
@@ -0,0 +1,12 @@
+public class Veiculo {
+    int passageiros;
+    double combustível;
+    double consumo;
+    // Teste 
+    public Veiculo(int pass, double comb, double cons)
+    {
+        passageiros = pass;
+        combustível = comb;
+        consumo = cons;
+    }
+}
